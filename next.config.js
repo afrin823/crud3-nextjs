39b2fb95ca98bc2b5140cbb437905b872068a21d
afrin823 <afrin12490@gmail.com>
2024-10-27 23:37:47 +0600

    dafdf

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com','avatars.githubusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  },
+  webpack(config, { isServer }) {
+    // Ensure compatibility for server and client-side builds
+    if (!isServer) {
+      config.resolve.fallback = { fs: false, path: false };
     }
-  }
-module.exports = nextConfig
+
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
